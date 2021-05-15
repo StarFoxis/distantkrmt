07a6fd6caaa84395903c33d648b4f3305721cc1e
StarFoxis <72461577+StarFoxis@users.noreply.github.com>
2021-05-15 17:18:48 +0500

    S

--- tasks/forms.py
+++ tasks/forms.py
@@ -21,10 +21,11 @@ class EditTaskForm(forms.ModelForm):
         fields = ('name', 'task', 'date_end', 'active', 'date_opening')
 
     def clean_date_opening(self):
+        active = self.cleaned_data['active']
         date_open = self.cleaned_data['date_opening']
         date_end = self.cleaned_data['date_end']
 
-        if (date_open and date_open >= date_end):
+        if (not active and date_open and date_open >= date_end):
             raise forms.ValidationError('Дата открытия не может быть меньше или равна даты закрытия')
 
         return date_open
@@ -48,10 +49,11 @@ class TaskForm(forms.ModelForm):
         self.fields['subject'].widget.attrs['disabled'] = 'disabled'
 
     def clean_date_opening(self):
+        active = self.cleaned_data['active']
         date_open = self.cleaned_data['date_opening']
         date_end = self.cleaned_data['date_end']
 
-        if (date_open and date_open >= date_end):
+        if (not active and date_open and date_open >= date_end):
             raise forms.ValidationError('Дата открытия не может быть меньше или равна даты закрытия')
 
         return date_open
